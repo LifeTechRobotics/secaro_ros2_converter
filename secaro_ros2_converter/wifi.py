3f28c5b080c48cb34ded1259c444392e03a792f0
motii <108280115+motii8128@users.noreply.github.com>
2025-08-09 20:48:37 +0900

    一部関数のモジュール化　＆　コメント文の追加　＆　車輪速度変更コマンドの送信部分実装

--- secaro_ros2_converter/wifi.py
+++ secaro_ros2_converter/wifi.py
@@ -5,6 +5,9 @@ from geometry_msgs.msg import Twist
 
 import socket
 
+# secaro用モジュール
+import secaro
+
 # UDP通信用
 UDP_PORT_CONTROL_BROADCAST = 4210
 UDP_PORT_CONTROL = 4211
@@ -42,66 +45,41 @@ class SecaroWiFiNode(Node):
         self.sock.setblocking(False)
 
         # 最終更新時の速度
-        self.prev_linear_velocity = 1
+        self.prev_linear_velocity = 0
 
         self.get_logger().info("Start %s " %self.get_name())
 
     def topic_callback(self, msg:Twist):
-        # どの条件にも当てはまらなかった場合「stop」を送信する
-        send_cmd = 'S\n'
+        """
+        Twist型メッセージを受信した際に呼び出される
 
-        # linear.y > 0　で前進するコマンド
-        if(msg.linear.y > 0.0):
-            send_cmd = 'F\n'
-
-        # linear.y < 0　で後退するコマンド
-        elif(msg.linear.y < 0.0):
-            send_cmd = 'B\n'
-
-        # angular.z > 0　で右旋回
-        elif(msg.angular.z > 0.0):
-            send_cmd = 'R\n'
-
-        # angular.z < 0　で左旋回
-        elif(msg.angular.z < 0.0):
-            send_cmd = 'L\n'
-
-        # 並進速度も旋回速度も入力がない場合、ストップするコマンド
-        # elif(msg.angular.z == 0.0 & msg.linear.y == 0.0):
-        #     send_cmd = 'S\n'
-
+        Parameters
+        ----------
+        msg: Twist
+            受信したROS2 Twist型メッセージ
+        """
+        
         # UDP送信
-        # 制御対象が見つかっていない場合は送信しない
+        # 制御対象が見つかっていない場合(==None)は送信しない
         if(self.target_ip != None):
-            try:
-                # 送信成功ログ(確認用)
-                if(self.enable_log):
-                    self.get_logger().info("IP: %s 送信: %s" % self.target_ip % send_cmd)
-                
-                self.sock.sendto(send_cmd.encode('utf-8'), (self.target_ip, UDP_PORT_CONTROL))
-            except Exception as e:
-                # エラーログ
-                self.get_logger().error('error content :%s' % e)
 
-            # 速度変更コマンドのUDP送信
-            if((self.prev_linear_velocity - abs(msg.linear.y)) != 0):
-                # 最終更新時の速度を更新
-                self.prev_linear_velocity = abs(msg.linear.y)
+            self.send_motion_cmd(msg=msg)
 
-                # 速度変更コマンドの作成
-                left_vel_cmd = 'l' + str(self.prev_linear_velocity) + '\n'
-                right_vel_cmd = 'l' + str(self.prev_linear_velocity) + '\n'
+            self.send_velocity_cmd(msg=msg)
 
-                # 左輪速度変更コマンドの送信
-                self.sock.sendto(left_vel_cmd.encode('utf-8'), (self.target_ip, UDP_PORT_CONTROL))
+            
+        else:
+            self.get_logger().error("No Device found yet.")
 
-                # 右輪速度変更コマンドの送信
-                self.sock.sendto(right_vel_cmd.encode('utf-8'), (self.target_ip, UDP_PORT_CONTROL))
-
-    # 0.1sごとに呼び出される。
     def timer_callback(self):
+        """
+        0.1秒ごとに呼び出される。
+        マイコンからのブロードキャストを受信する
+        """
+
         if(self.target_ip == None):
             self.get_logger().info("Scan Device ...")
+
         try:
             data, addr = self.sock.recvfrom(1024)
             msg = data.decode()
@@ -111,6 +89,65 @@ class SecaroWiFiNode(Node):
                 self.get_logger().info("Found Device. name:%s ip:%s" %name %ip)
         except BlockingIOError:
             pass
+
+    def send_motion_cmd(self, msg:Twist):
+        """
+        動作コマンドを送信する
+
+        Parameters
+        ----------
+        msg: Twist
+            ROS2 Twist型メッセージ
+        """
+        # Twistから動作コマンド（前進、後退、右旋回、左旋回）を取得
+        send_cmd = secaro.get_motion_cmd(linear_velocity=msg.linear.y, angular_velocity=msg.angular.z)
+
+        self.udp_send(send_cmd=send_cmd)
+
+    def send_velocity_cmd(self, msg:Twist):
+        """
+        車輪速度コマンドを送信する
+
+        Parameters
+        ----------
+        msg: Twist
+            ROS2 Twist型メッセージ
+        """
+        # Twistから速度コマンドを取得(1~9)
+        velocity_cmd = secaro.get_velocity_cmd(vel=msg.linear.y)
+        # 速度変更コマンドのUDP送信
+        # 前回の速度と変更がある場合のみ送信
+        # prev_linear_velocityは０で初期化されてるので初回は必ず速度コマンドを送信する
+        if((self.prev_linear_velocity - velocity_cmd) != 0):
+            # 最終更新時の速度を更新
+            self.prev_linear_velocity = velocity_cmd
+            # 速度変更コマンドの作成
+            left_vel_cmd = 'l' + str(self.prev_linear_velocity) + '\n'
+            right_vel_cmd = 'r' + str(self.prev_linear_velocity) + '\n'
+            # 左輪速度変更コマンドの送信
+            self.udp_send(send_cmd=left_vel_cmd)
+            # 右輪速度変更コマンドの送信
+            self.udp_send(send_cmd=right_vel_cmd)
+
+    def udp_send(self, send_cmd:str):
+        """
+        UDP送信をする
+
+        Parameters
+        ----------
+        send_cmd: str
+            送信データ
+        """
+        try:
+            # 送信成功ログ(確認用)
+            if(self.enable_log):
+                self.get_logger().info("IP: %s. send content: %s" % self.target_ip % send_cmd)
+            
+            self.sock.sendto(send_cmd.encode('utf-8'), (self.target_ip, UDP_PORT_CONTROL))
+        except Exception as e:
+            # エラーログ
+            self.get_logger().error('error content :%s' % e)
+
 
 def main(args=None):
     rclpy.init(args=args)
